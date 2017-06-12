90edac80776bbbdd76f20323a26433e1cc8a5215
danny <daniel.toone@gmail.com>
2017-06-12 15:00:47 -0700

    fix route props

--- src/partier-app/profile/index.tsx
+++ src/partier-app/profile/index.tsx
@@ -1,18 +1,73 @@
 import * as React from "react";
-import { RouteComponentProps, Switch, Route } from 'react-router-dom';
+import { RouteComponentProps, Route, Link } from 'react-router-dom';
 import { observer } from "mobx-react";
+import * as _ from 'lodash';
 import ProfileModel from "./ProfileModel";
-import ShowProfile from "./ShowProfile";
-import EditProfile from "./EditProfile";
+import EditProfile from "./edit";
 import './styles.scss';
+
+
+export interface IProfileModel {
+	profile: ProfileModel;
+}
 
 @observer
 export default class Profile extends React.Component<RouteComponentProps<any>, {}> {
 	profile = new ProfileModel;
-	render() {
-		return <Switch>
-			<Route path="/partier/profile/edit" render={(props) => <EditProfile profile={this.profile}/>} />
-			<Route render={(props) => <ShowProfile profile={this.profile}/>} />
-		</Switch>;
+
+	renderOtherPhotos() {
+		return (
+  		<div className="other-photos-container">
+  			{
+  				_.range(2).map((row_index: number) => (
+		  			<div className="other-photos-row" key={`other_photos_row_${row_index}`}>
+		  			{
+		  				_.range(5).map((col_index: number) => (
+		  					<div className="other-photos-col" key={`other_photos_col_${col_index}`}>
+			  				</div>
+		  				))
+		  			}
+		  			</div>
+		  		))
+	  		}
+  		</div>
+		);
 	}
+
+	clickPhoto() {
+
+	}
+
+  render() {
+	return <div className="profile-wrapper">
+		<div className="profile-contents">
+			<div className="profile-top-contents">
+				<div className="photo-container">
+					<Link to={`/partier/profile/edit`}>
+						<div className="main-photo" 
+							style={{backgroundImage: `url(${this.profile.photos.slice(0)})`}} 
+							onClick={this.clickPhoto}/>
+					</Link>
+					{this.renderOtherPhotos()}
+				</div>
+				<div className="profile-list">
+					{this.profile.name}<br />
+					{this.profile.home}<br />
+					{this.profile.gender}<br />
+				</div>
+			</div>
+			<br />
+			<div className="friend-list">
+					<ul className="list-group">
+					  <li className="list-group-item">Cras justo odio</li>
+					  <li className="list-group-item">Dapibus ac facilisis in</li>
+					  <li className="list-group-item">Morbi leo risus</li>
+					  <li className="list-group-item">Porta ac consectetur ac</li>
+					  <li className="list-group-item">Vestibulum at eros</li>
+					</ul>
+			</div>
+		</div>
+			<Route path="/partier/profile/edit" render={(props) => <EditProfile profile={this.profile}/>} />
+	</div>;
+  }
 }
