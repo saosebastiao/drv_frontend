e4260757594e8dee4a818fe984753c059613fc4c
danny <daniel.toone@gmail.com>
2017-06-12 18:01:17 -0700

    snapshot

--- src/partier-app/profile/index.tsx
+++ src/partier-app/profile/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { RouteComponentProps, Switch, Route, Link } from 'react-router-dom';
 import { observer } from "mobx-react";
-import * as _ from 'lodash';
 import EditProfile from "./edit";
 import ShowProfile from "./default";
 import './styles.scss';
@@ -10,29 +9,6 @@ import './styles.scss';
 
 @observer
 export default class Profile extends React.Component<RouteComponentProps<any>, {}> {
-
-	renderOtherPhotos() {
-		return (
-			<div className="other-photos-container">
-				{
-					_.range(2).map((row_index: number) => (
-						<div className="other-photos-row" key={`other_photos_row_${row_index}`}>
-							{
-								_.range(5).map((col_index: number) => (
-									<div className="other-photos-col" key={`other_photos_col_${col_index}`}>
-									</div>
-								))
-							}
-						</div>
-					))
-				}
-			</div>
-		);
-	}
-
-	clickPhoto() {
-
-	}
 
 	render() {
 		return <Switch>
