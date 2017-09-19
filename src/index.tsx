1971356c9cccda2d18a48f69aee2cb0d244635c2
danny <daniel.toone@gmail.com>
2017-09-19 12:54:49 -0700

    tslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,27 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import * as React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Admin from "./admin-app";
 import Home from "./home";
 import Partier from "./partier-app";
 import Promoter from "./promoter-app";
-import Admin from "./admin-app";
 
 import DevTool from "mobx-react-devtools";
 
-
 import "./styles.scss";
 
-
 class AppRouter extends React.Component<{}, {}> {
-  render() {
+  public render() {
     return (
-        <BrowserRouter>
-					<Switch>
-						<Route exact={true} path="/" component={Home} />
-						<Route path="/partier" component={Partier} />
-						<Route path="/promoter" component={Promoter} />
-						<Route path="/admin" component={Admin} />
-					</Switch>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/partier" component={Partier} />
+          <Route path="/promoter" component={Promoter} />
+          <Route path="/admin" component={Admin} />
+        </Switch>
+      </BrowserRouter>
     );
   }
-};
-render(<AppRouter />, document.getElementById('root'));
+}
+render(<AppRouter />, document.getElementById("root"));
