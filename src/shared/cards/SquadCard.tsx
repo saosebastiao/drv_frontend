7aa9e12f19bc0c367ae1935e2320c9e131e0d256
danny <daniel.toone@gmail.com>
2017-09-19 10:20:45 -0700

    tslint

--- src/shared/cards/SquadCard.tsx
+++ src/shared/cards/SquadCard.tsx
@@ -1,45 +1,47 @@
-import * as React from 'react';
-import { observable, computed, runInAction } from "mobx";
+import { computed, observable, runInAction } from "mobx";
 import { observer } from "mobx-react";
 import { getSquad } from "modules/DroverClient";
+import * as React from "react";
 import PartierCard from "./PartierCard";
 
 class SquadCardModel {
-	constructor(public squadID: number) {
-		this.refresh();
-	}
-	@observable squadName: string;
-	@observable ownerID: string;
-	@observable auction: IAuction;
-	@observable filters?: ISquadFilters;
-	@observable squadMembers: Array<ISquadMember> = [];
-	@computed get isReady() {
-		return this.squadName != null;
-	}
-	async refresh() {
-		const s = await getSquad(this.squadID);
-		runInAction(() => {
-			Object.assign(this, s);
-		});
-	}
+  constructor(public squadID: number) {
+    this.refresh();
+  }
+  @observable public squadName: string;
+  @observable public ownerID: string;
+  @observable public auction: IAuction;
+  @observable public filters?: ISquadFilters;
+  @observable public squadMembers: Array<ISquadMember> = [];
+  @computed get isReady() {
+    return this.squadName != null;
+  }
+  public async refresh() {
+    const s = await getSquad(this.squadID);
+    runInAction(() => {
+      Object.assign(this, s);
+    });
+  }
 }
 
 interface PSquadCard {
-	squadID: number;
+  squadID: number;
 }
 
 @observer
 export default class SquadCard extends React.Component<PSquadCard, {}>{
-	model = new SquadCardModel(this.props.squadID);
-	render() {
-		return this.model.isReady ? <div>
-			<div>Squad Name: {this.model.squadName}</div>
-			<div>Owner: <PartierCard userID={this.model.ownerID} /></div>
-			<div>Members:
-				{this.model.squadMembers.map(x => {
-					return x.accepted ? <PartierCard userID={x.userID} /> : null;
-				})}
-			</div>
-		</div> : null;
-	}
+  public model = new SquadCardModel(this.props.squadID);
+  public render() {
+    return this.model.isReady ? (
+      <div>
+        <div>Squad Name: {this.model.squadName}</div>
+        <div>Owner: <PartierCard userID={this.model.ownerID} /></div>
+        <div>Members:
+        {this.model.squadMembers.map((x) => {
+            return x.accepted ? <PartierCard userID={x.userID} /> : null;
+          })}
+        </div>
+      </div>
+    ) : null;
+  }
 }
