e53ca2938addda6155d2d674633380178eb0925a
danny <daniel.toone@gmail.com>
2017-09-21 16:29:15 -0700

    snapshot

--- src/partier-app/squad/$squadID/edit/Model.ts
+++ src/partier-app/squad/$squadID/edit/Model.ts
@@ -13,10 +13,9 @@ export default class EditSquadModel {
     return this.squad != null;
   }
   @observable public squadName: string;
-  @observable public venueBlacklist: Set<number> = new Set();
-  @observable public musicTypes: Set<string> = new Set();
-  @observable public venueTypes: Set<string> = new Set();
-  @observable public interactionTypes: Set<string> = new Set();
+  @observable public venueBlacklist: Map<number, void> = new Map();
+  @observable public venueTypes: Map<string, boolean> = new Map();
+  @observable public interactionTypes: Map<string, boolean> = new Map;
   @observable public squadMembers: Array<ISquadMember>;
   @computed get potential() {
     return this.squadMembers
