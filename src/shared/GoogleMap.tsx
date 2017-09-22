717de8e5bd6fb01701142b0855fb9d6b8bd20eaa
danny <daniel.toone@gmail.com>
2017-09-22 12:12:54 -0700

    snapshot

--- src/shared/GoogleMap.tsx
+++ src/shared/GoogleMap.tsx
@@ -1,21 +1,47 @@
 import * as React from "react";
 
 interface PGoogleMap {
-  tmp?: string;
+  center?: google.maps.LatLng;
+  zoom?: number;
+  region?: google.maps.Data.MultiPolygon;
+  chosen?: google.maps.Data.Point;
+  venues?: Array<google.maps.Data.Point>;
 }
-export default class GoogleMap extends React.Component<PGoogleMap, {}>{
-  private map: google.maps.Map;
 
+export default class GoogleMap extends React.Component<PGoogleMap, {}>{
+  private map: google.maps.Map | null = null;
   private latlng = new google.maps.LatLng(-34.397, 150.644);
-  public componentDidMount() {
-    const mapOptions: google.maps.MapOptions = {
-      zoom: 8,
+  private zoom = 8;
+  private get mapOptions(): google.maps.MapOptions {
+    return {
+      zoom: this.zoom,
       center: this.latlng
     };
-
+  }
+  public shouldComponentUpdate() {
+    return false;
+  }
+  public componentWillReceiveProps(next: PGoogleMap) {
+    // tslint:disable-next-line:no-console
+    console.log(next);
+    if (this.map) {
+      if (next.center) {
+        this.map.setCenter(next.center);
+      }
+      if (next.zoom) {
+        this.map.setZoom(next.zoom);
+      }
+      if (next.region) {
+        this.map.data.add({ geometry: next.region });
+      }
+    }
+  }
+  public componentDidMount() {
     const elem = document.getElementById("google-map");
-    this.map = new google.maps.Map(elem, mapOptions);
-
+    this.map = new google.maps.Map(elem, this.mapOptions);
+  }
+  public componentWillUnmount() {
+    this.map = null;
   }
   public render() {
     return <div id="google-map" />;
