35e2e051244ddd37e21ed3e68510a1d32874f7cc
danny <daniel.toone@gmail.com>
2017-11-21 14:24:05 -0800

    snapshot

---
+++ src/partier-app/squad/$squadID/default/index.tsx
@@ -0,0 +1,222 @@
+import { observer } from "mobx-react";
+import * as React from "react";
+import { Link, RouteComponentProps } from "react-router-dom";
+import PartierCard from "shared/cards/PartierCard";
+import ViewSquadModel from "./Model";
+
+interface PViewSquad {
+  squadID: string;
+}
+
+@observer
+export default class ViewSquad extends React.Component<RouteComponentProps<PViewSquad>, {}> {
+  public model: ViewSquadModel;
+  constructor(props: RouteComponentProps<PViewSquad>) {
+    super(props);
+    const squadID = parseInt(props.match.params.squadID, 10);
+    this.model = new ViewSquadModel(squadID);
+  }
+
+  public render() {
+    if (this.model.isReady) {
+      return (
+        <div className="squad-wrapper">
+          <div className="squad-details-contents">
+            <div className="squad-details-row">
+              <div className="details-col">
+                <div className="info-wrapper">
+                  <div className="info-title">Squad Information</div>
+                  <div className="info-row">
+                    <div className="info-label">Name</div>
+                    <div className="info-value">{this.model.squadName}</div>
+                  </div>
+                  <div className="info-row">
+                    <div className="info-label">Owner</div>
+                    <PartierCard userID={this.model.ownerID} />
+                  </div>
+                  <div className="info-title">Auction Information</div>
+                  <div className="info-row">
+                    <div className="info-label">City</div>
+                    <div className="info-value">{this.model.auction.regionID}</div>
+                  </div>
+                  <div className="info-row">
+                    <div className="info-label">Party Night</div>
+                    <div className="info-value">{this.model.auction.partyNight}</div>
+                  </div>
+                  <div className="info-row">
+                    <div className="info-label">Auction Start Time</div>
+                    <div className="info-value">{this.model.auction.startTime}</div>
+                  </div>
+                  <div className="info-row">
+                    <div className="info-value">
+                      <Link to={`/partier/squad/${this.model.squadID}/auction`}>
+                        <button
+                          type="button"
+                          className="btn btn-xs btn-primary"
+                        >
+                          Go To Auction Page
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                  {this.model.isOwned ? (
+                    <div className="info-row">
+                      <div className="info-value">
+                        <Link to={`/partier/squad/${this.model.squadID}/edit`}>
+                          <button
+                            type="button"
+                            className="btn btn-xs btn-primary">
+                            Edit Squad Name
+                          </button>
+                        </Link>
+                      </div>
+                    </div>
+                  ) : null}
+                  {this.model.isOwned ? (
+                    <div className="info-row">
+                      <div className="info-value">
+                        <Link to={`/partier/squad/${this.model.squadID}/members`}>
+                          <button
+                            type="button"
+                            className="btn btn-xs btn-primary">
+                            Invite or Manage Squad Members
+                        </button>
+                        </Link>
+                      </div>
+                    </div>
+                  ) : null}
+                  {this.model.isOwned ? (
+                    <div className="info-row">
+                      <div className="info-value">
+                        <Link to={`/partier/squad/${this.model.squadID}/filters`}>
+                          <button
+                            type="button"
+                            className="btn btn-xs btn-primary">
+                            Manage Squad Filters
+                        </button>
+                        </Link>
+                      </div>
+                    </div>
+                  ) : null}
+                  {this.model.isOwned ? (
+                    <div className="info-row">
+                      <div className="info-value">
+                        <Link to={`/partier/squad/${this.model.squadID}/payouts`}>
+                          <button
+                            type="button"
+                            className="btn btn-xs btn-primary">
+                            Manage Squad Payouts
+                        </button>
+                        </Link>
+                      </div>
+                    </div>
+                  ) : null}
+                  {this.model.myself && this.model.myself.accepted || this.model.isOwned ? (
+                    <div className="info-row">
+                      <div className="info-value">
+                        <Link to={`/partier/squad/${this.model.squadID}/social`}>
+                          <button
+                            type="button"
+                            className="btn btn-xs btn-primary">
+                            Manage Squad Social Media Commitments
+                        </button>
+                        </Link>
+                      </div>
+                    </div>
+                  ) : null}
+                  {this.model.winningBid ? (
+                    <div className="info-row">
+                      <div className="info-value">
+                        <Link to={`/partier/squad/${this.model.squadID}/party`}>
+                          <button
+                            type="button"
+                            className="btn btn-xs btn-primary">
+                            View Party Assignment
+                          </button>
+                        </Link>
+                      </div>
+                    </div>
+                  ) : null}
+                  {this.model.isOwned ? (
+                    <div className="info-row">
+                      <div className="info-value">
+                        <button
+                          type="button"
+                          onClick={() => alert("Not Implemented")}
+                          className="btn btn-xs btn-primary">
+                          Delete Squad
+                          </button>
+                      </div>
+                    </div>
+                  ) : null}
+                </div>
+              </div>
+              <div className="details-col has-border">
+                <div className="member-wrapper">
+                  {this.model.accepted.length > 0 ? <div>Accepted</div> : null}
+                  {this.model.accepted.map((x) => {
+                    return (
+                      <PartierCard key={x} userID={x}>
+                        {this.model.isSelf(x) ? (
+                          <button
+                            type="button"
+                            className="btn btn-xs btn-primary"
+                            onClick={this.model.rejectInvite}>
+                            Reject
+                          </button>
+                        ) : null}
+                      </PartierCard>
+                    );
+                  })}
+                  {this.model.invited.length > 0 ? <div>Invited</div> : null}
+                  {this.model.invited.map((x) => {
+                    return (
+                      <PartierCard key={x} userID={x}>
+                        {this.model.isSelf(x) ? (
+                          <button
+                            type="button"
+                            className="btn btn-xs btn-primary"
+                            onClick={this.model.acceptInvite}
+                          >
+                            Accept
+                          </button>
+                        ) : null}
+                        {this.model.isSelf(x) ? (
+                          <button
+                            type="button"
+                            className="btn btn-xs btn-primary"
+                            onClick={this.model.rejectInvite}
+                          >
+                            Reject
+                          </button>
+                        ) : null}
+                      </PartierCard>
+                    );
+                  })}
+                  {this.model.rejected.length > 0 ? <div>Rejected</div> : null}
+                  {this.model.rejected.map((x) => {
+                    return (
+                      <PartierCard key={x} userID={x}>
+                        {this.model.isSelf(x) ? (
+                          <button
+                            type="button"
+                            className="btn btn-xs btn-primary"
+                            onClick={this.model.acceptInvite}
+                          >
+                            Accept
+                          </button>
+                        ) : null}
+                      </PartierCard>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
--- src/partier-app/squad/$squadID/index.tsx
+++ src/partier-app/squad/$squadID/index.tsx
@@ -1,222 +1,31 @@
-import { observer } from "mobx-react";
 import * as React from "react";
-import { Link, RouteComponentProps } from "react-router-dom";
-import PartierCard from "shared/cards/PartierCard";
-import ViewSquadModel from "./Model";
+import { Route, RouteComponentProps, Switch } from "react-router-dom";
+import EditSquad from "./edit";
+import EditFilters from "./filters";
+import EditMembers from "./members";
+import EditPayouts from "./payouts";
+import EditSocial from "./social";
+import SquadList from "./default";
+import ViewAuction from "./auction";
+import ViewParty from "./party";
 
-interface PViewSquad {
-  squadID: string;
-}
-
-@observer
-export default class ViewSquad extends React.Component<RouteComponentProps<PViewSquad>, {}> {
-  public model: ViewSquadModel;
-  constructor(props: RouteComponentProps<PViewSquad>) {
-    super(props);
-    const squadID = parseInt(props.match.params.squadID, 10);
-    this.model = new ViewSquadModel(squadID);
-  }
+export default class Squad extends React.Component<RouteComponentProps<any>, {}> {
 
   public render() {
-    if (this.model.isReady) {
-      return (
-        <div className="squad-wrapper">
-          <div className="squad-details-contents">
-            <div className="squad-details-row">
-              <div className="details-col">
-                <div className="info-wrapper">
-                  <div className="info-title">Squad Information</div>
-                  <div className="info-row">
-                    <div className="info-label">Name</div>
-                    <div className="info-value">{this.model.squadName}</div>
-                  </div>
-                  <div className="info-row">
-                    <div className="info-label">Owner</div>
-                    <PartierCard userID={this.model.ownerID} />
-                  </div>
-                  <div className="info-title">Auction Information</div>
-                  <div className="info-row">
-                    <div className="info-label">City</div>
-                    <div className="info-value">{this.model.auction.regionID}</div>
-                  </div>
-                  <div className="info-row">
-                    <div className="info-label">Party Night</div>
-                    <div className="info-value">{this.model.auction.partyNight}</div>
-                  </div>
-                  <div className="info-row">
-                    <div className="info-label">Auction Start Time</div>
-                    <div className="info-value">{this.model.auction.startTime}</div>
-                  </div>
-                  <div className="info-row">
-                    <div className="info-value">
-                      <Link to={`/partier/squad/${this.model.squadID}/auction`}>
-                        <button
-                          type="button"
-                          className="btn btn-xs btn-primary"
-                        >
-                          Go To Auction Page
-                        </button>
-                      </Link>
-                    </div>
-                  </div>
-                  {this.model.isOwned ? (
-                    <div className="info-row">
-                      <div className="info-value">
-                        <Link to={`/partier/squad/${this.model.squadID}/edit`}>
-                          <button
-                            type="button"
-                            className="btn btn-xs btn-primary">
-                            Edit Squad Name
-                          </button>
-                        </Link>
-                      </div>
-                    </div>
-                  ) : null}
-                  {this.model.isOwned ? (
-                    <div className="info-row">
-                      <div className="info-value">
-                        <Link to={`/partier/squad/${this.model.squadID}/members`}>
-                          <button
-                            type="button"
-                            className="btn btn-xs btn-primary">
-                            Invite or Manage Squad Members
-                        </button>
-                        </Link>
-                      </div>
-                    </div>
-                  ) : null}
-                  {this.model.isOwned ? (
-                    <div className="info-row">
-                      <div className="info-value">
-                        <Link to={`/partier/squad/${this.model.squadID}/filters`}>
-                          <button
-                            type="button"
-                            className="btn btn-xs btn-primary">
-                            Manage Squad Filters
-                        </button>
-                        </Link>
-                      </div>
-                    </div>
-                  ) : null}
-                  {this.model.isOwned ? (
-                    <div className="info-row">
-                      <div className="info-value">
-                        <Link to={`/partier/squad/${this.model.squadID}/payouts`}>
-                          <button
-                            type="button"
-                            className="btn btn-xs btn-primary">
-                            Manage Squad Payouts
-                        </button>
-                        </Link>
-                      </div>
-                    </div>
-                  ) : null}
-                  {this.model.myself && this.model.myself.accepted || this.model.isOwned ? (
-                    <div className="info-row">
-                      <div className="info-value">
-                        <Link to={`/partier/squad/${this.model.squadID}/social`}>
-                          <button
-                            type="button"
-                            className="btn btn-xs btn-primary">
-                            Manage Squad Social Media Commitments
-                        </button>
-                        </Link>
-                      </div>
-                    </div>
-                  ) : null}
-                  {this.model.winningBid ? (
-                    <div className="info-row">
-                      <div className="info-value">
-                        <Link to={`/partier/squad/${this.model.squadID}/party`}>
-                          <button
-                            type="button"
-                            className="btn btn-xs btn-primary">
-                            View Party Assignment
-                          </button>
-                        </Link>
-                      </div>
-                    </div>
-                  ) : null}
-                  {this.model.isOwned ? (
-                    <div className="info-row">
-                      <div className="info-value">
-                        <button
-                          type="button"
-                          onClick={() => alert("Not Implemented")}
-                          className="btn btn-xs btn-primary">
-                          Delete Squad
-                          </button>
-                      </div>
-                    </div>
-                  ) : null}
-                </div>
-              </div>
-              <div className="details-col has-border">
-                <div className="member-wrapper">
-                  {this.model.accepted.length > 0 ? <div>Accepted</div> : null}
-                  {this.model.accepted.map((x) => {
-                    return (
-                      <PartierCard key={x} userID={x}>
-                        {this.model.isSelf(x) ? (
-                          <button
-                            type="button"
-                            className="btn btn-xs btn-primary"
-                            onClick={this.model.rejectInvite}>
-                            Reject
-                          </button>
-                        ) : null}
-                      </PartierCard>
-                    );
-                  })}
-                  {this.model.invited.length > 0 ? <div>Invited</div> : null}
-                  {this.model.invited.map((x) => {
-                    return (
-                      <PartierCard key={x} userID={x}>
-                        {this.model.isSelf(x) ? (
-                          <button
-                            type="button"
-                            className="btn btn-xs btn-primary"
-                            onClick={this.model.acceptInvite}
-                          >
-                            Accept
-                          </button>
-                        ) : null}
-                        {this.model.isSelf(x) ? (
-                          <button
-                            type="button"
-                            className="btn btn-xs btn-primary"
-                            onClick={this.model.rejectInvite}
-                          >
-                            Reject
-                          </button>
-                        ) : null}
-                      </PartierCard>
-                    );
-                  })}
-                  {this.model.rejected.length > 0 ? <div>Rejected</div> : null}
-                  {this.model.rejected.map((x) => {
-                    return (
-                      <PartierCard key={x} userID={x}>
-                        {this.model.isSelf(x) ? (
-                          <button
-                            type="button"
-                            className="btn btn-xs btn-primary"
-                            onClick={this.model.acceptInvite}
-                          >
-                            Accept
-                          </button>
-                        ) : null}
-                      </PartierCard>
-                    );
-                  })}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      );
-    } else {
-      return null;
-    }
+    return (
+      <div className="squad-wrapper">
+        <Switch>
+          <Route exact path="/partier/squad" component={SquadList} />
+          <Route path="/partier/squad/:squadID/edit" component={EditSquad} />
+          <Route path="/partier/squad/:squadID/filters" component={EditFilters} />
+          <Route path="/partier/squad/:squadID/members" component={EditMembers} />
+          <Route path="/partier/squad/:squadID/payouts" component={EditPayouts} />
+          <Route path="/partier/squad/:squadID/social" component={EditSocial} />
+          <Route path="/partier/squad/:squadID/auction" component={ViewAuction} />
+          <Route path="/partier/squad/:squadID/party" component={ViewParty} />
+        </Switch>
+      </div>
+    );
   }
+
 }
